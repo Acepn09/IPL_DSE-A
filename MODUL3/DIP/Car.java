9e6f3e3d11ee75618bb1563bfaeebc083653acc6
acep nugraha <nacepnnacepn92@gmail.com>
2024-11-13 14:45:59 +0700

    After

--- MODUL3/DIP/Car.java
+++ MODUL3/DIP/Car.java
@@ -1,7 +1,7 @@
 class Car {
-    private final Engine engine;
+    private final EngineInterface engine;
 
-    public Car(Engine engine) {
+    public Car(EngineInterface engine) {
         this.engine = engine;
     }
 
